82e88a44c65f2065d2dad088ebc2e01c08544bc0
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-02-29 15:16:25 -0300

    android multi screen event listener em imagens

--- android/EletricCarApp/app/src/main/java/me/dio/android/eletriccarapp/presentation/AutonomyCalculatorActivity.kt
+++ android/EletricCarApp/app/src/main/java/me/dio/android/eletriccarapp/presentation/AutonomyCalculatorActivity.kt
@@ -3,21 +3,22 @@ package me.dio.android.eletriccarapp.presentation
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import me.dio.android.eletriccarapp.R
 import java.math.RoundingMode
 
 class AutonomyCalculatorActivity : AppCompatActivity() {
-    lateinit var pricePerKwh : EditText;
-    lateinit var kmTraveler: EditText;
-    lateinit var btnCalculate : Button;
-    lateinit var autonomyResult: TextView;
+    lateinit var pricePerKwh : EditText
+    lateinit var kmTraveler: EditText
+    lateinit var btnCalculate : Button
+    lateinit var autonomyResult: TextView
+    lateinit var btnClose: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_autonomy_calculator)
-
         setupView()
         setupListeners()
     }
@@ -27,11 +28,15 @@ class AutonomyCalculatorActivity : AppCompatActivity() {
         kmTraveler = findViewById(R.id.et_km_traveled)
         btnCalculate = findViewById<Button>(R.id.btn_calculate)
         autonomyResult = findViewById(R.id.tv_autonomy_result)
+        btnClose = findViewById(R.id.iv_close)
     }
 
     fun setupListeners() {
         btnCalculate.setOnClickListener {
             calculate()
+        }
+        btnClose.setOnClickListener {
+            finish()
         }
     }
 
