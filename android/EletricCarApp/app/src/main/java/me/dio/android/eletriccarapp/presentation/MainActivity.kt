0bf574eb328df23be83c2821f1539ab7553a2ec7
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-03-01 14:43:18 -0300

    android multi screen recycler view with adapter

--- android/EletricCarApp/app/src/main/java/me/dio/android/eletriccarapp/presentation/MainActivity.kt
+++ android/EletricCarApp/app/src/main/java/me/dio/android/eletriccarapp/presentation/MainActivity.kt
@@ -3,14 +3,16 @@ package me.dio.android.eletriccarapp.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.ListView
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import me.dio.android.eletriccarapp.R
+import me.dio.android.eletriccarapp.presentation.adapter.CarAdapter
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnCalculatorRedirect : Button
-    lateinit var list: ListView
+    lateinit var carsList: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     fun setupView() {
         btnCalculatorRedirect = findViewById(R.id.btn_calculator_redirect)
-        list = findViewById(R.id.lv_informations)
+        carsList = findViewById(R.id.rv_cars_list)
     }
 
     fun setupListeners() {
@@ -33,7 +35,7 @@ class MainActivity : AppCompatActivity() {
 
     fun setupList() {
         var data = arrayOf("Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich", "Jelly Bean", "KitKat", "Lollipop", "Marshmallow", "Nougat", "Oreo", "Pie")
-        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
-        list.adapter = adapter
+        var adapter = CarAdapter(data)
+        carsList.adapter = adapter
     }
 }
