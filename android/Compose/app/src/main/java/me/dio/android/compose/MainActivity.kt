98a022e5191b01d27f9eb4a25f37579c883bc53b
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-03-26 13:21:44 -0300

    androidx compose arranjo e alinhamento

--- android/Compose/app/src/main/java/me/dio/android/compose/MainActivity.kt
+++ android/Compose/app/src/main/java/me/dio/android/compose/MainActivity.kt
@@ -3,18 +3,28 @@ package me.dio.android.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.dio.android.compose.ui.theme.ComposeTheme
@@ -39,7 +49,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    Column(verticalArrangement = Arrangement.Center) {
+        DogCard(dog = Dog("Luna", "Chow Chow"), modifier = Modifier.background(Color.Green))
 
+        DogCard(dog = Dog("Zoey", "Pit Bull"), modifier = Modifier.background(Color.Yellow))
+    }
+}
+
+data class Dog(
+    val name: String,
+    val breed: String
+)
+
+@Composable
+private fun DogCard(dog: Dog, modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_dog),
+            contentDescription = "Dog image",
+            modifier = Modifier
+                .size(72.dp)
+                .clip(CircleShape)
+        )
+
+        Column {
+            Text(text = dog.name)
+
+            Text(text = dog.breed)
+        }
+    }
+}
+
+@Composable
+private fun TestingColumnRow() {
     Column(
         modifier = Modifier
             .background(Color.Red)
@@ -69,22 +115,25 @@ fun MainScreen() {
             Text(text = "Texto 4")
         }
     }
+}
 
-//    Text(text = "Outro texto qualquer")
-//    Button(
-//        onClick = { /* TO DO */ },
-//        colors = ButtonDefaults.buttonColors(
-//            containerColor = Color.Yellow,
-//            contentColor = Color.Black
-//        )
-//    ) {
-//        Icon(
-//            painter = painterResource(id = R.drawable.ic_award_star),
-//            contentDescription = "Star",
-//            tint = Color.Red
-//        )
-//        Text(text = "Confirmar", color = Color.Blue)
-//    }
+@Composable
+private fun TestingButton() {
+    Text(text = "Outro texto qualquer")
+    Button(
+        onClick = { /* TO DO */ },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Yellow,
+            contentColor = Color.Black
+        )
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_award_star),
+            contentDescription = "Star",
+            tint = Color.Red
+        )
+        Text(text = "Confirmar", color = Color.Blue)
+    }
 }
 
 @Preview(showBackground = true)
@@ -93,13 +142,13 @@ private fun MainScreenPreview() {
     MainScreen()
 }
 
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
 //@Composable
 //@Preview(showBackground = true)
