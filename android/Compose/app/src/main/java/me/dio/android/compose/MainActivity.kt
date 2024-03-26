d2a4b154fa45a1634be6bba5e28a7ba1bbc506b3
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-03-25 23:10:06 -0300

    androidx compose composable functions

--- android/Compose/app/src/main/java/me/dio/android/compose/MainActivity.kt
+++ android/Compose/app/src/main/java/me/dio/android/compose/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import me.dio.android.compose.ui.theme.ComposeTheme
 
@@ -30,6 +33,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MainScreen() {
+//    Text(text = "Meu primeiro texto com Compose")
+//    Text(text = "Outro texto qualquer")
+    Button(onClick = { /*TODO*/ }) {
+        Icon(painter = painterResource(id = R.drawable.ic_award_star), contentDescription = "Star")
+        Text(text = "Confirmar")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainScreenPreview() {
+    MainScreen()
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
@@ -37,10 +56,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Composable
-@Preview(showBackground = true)
-fun GreetingPreview() {
-    ComposeTheme {
-        Greeting("Rudolf")
-    }
-}
+//@Composable
+//@Preview(showBackground = true)
+//fun GreetingPreview() {
+//    ComposeTheme {
+//        Greeting("Rudolf")
+//    }
+//}
